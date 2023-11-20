1a87b1611d3f67f1876d1023ac429a1406551581
Odyn666 <lambertmorhen@wp.pl>
2023-11-20 17:58:26 +0100

    Initial commit

---
+++ src/main/java/pl/khas/SdaLibraryProject/SdaLibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.khas.SdaLibraryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaLibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaLibraryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/khas/SdaLibraryProject/SdaLibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.khas.SdaLibraryProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaLibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
